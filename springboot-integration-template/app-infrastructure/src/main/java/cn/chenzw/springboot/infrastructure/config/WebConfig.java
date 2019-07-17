29acd802e6f58edc02288aa005a53466964d1c3a
chenzw <656469722@qq.com>
2019-07-17 20:03:30 +0800

    initialization

--- springboot-integration-template/app-infrastructure/src/main/java/cn/chenzw/springboot/infrastructure/config/WebConfig.java
+++ springboot-integration-template/app-infrastructure/src/main/java/cn/chenzw/springboot/infrastructure/config/WebConfig.java
@@ -1,6 +1,5 @@
 package cn.chenzw.springboot.infrastructure.config;
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
