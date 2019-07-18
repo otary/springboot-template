78cd60fc2b0bc19ed3343121ecac609e9ac70776
chenzw <656469722@qq.com>
2019-07-18 21:36:03 +0800

    initialization

---
+++ springboot-integration-template/app-infrastructure/src/main/java/cn/chenzw/springboot/infrastructure/domain/entity/SysConfig.java
@@ -0,0 +1,9 @@
+package cn.chenzw.springboot.infrastructure.domain.entity;
+
+/**
+ * 系统配置表
+ *
+ * @author chenzw
+ */
+public class SysConfig {
+}
---
+++ springboot-integration-template/app-infrastructure/src/main/java/cn/chenzw/springboot/infrastructure/domain/entity/SysUser.java
@@ -0,0 +1,4 @@
+package cn.chenzw.springboot.infrastructure.domain.entity;
+
+public class SysUser {
+}
