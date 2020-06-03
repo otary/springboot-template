30935c8f87f5621142ec472982113add95c9b51e
chenzw <656469722@qq.com>
2020-06-03 19:17:52 +0800

    add 全局时区配置

--- springboot-integration-template/app-infrastructure/src/main/java/cn/chenzw/springboot/infrastructure/config/AppConfig.java
+++ springboot-integration-template/app-infrastructure/src/main/java/cn/chenzw/springboot/infrastructure/config/AppConfig.java
@@ -1,8 +1,11 @@
 package cn.chenzw.springboot.infrastructure.config;
 
 import org.dozer.spring.DozerBeanMapperFactoryBean;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.TimeZone;
 
 @Configuration
 public class AppConfig {
@@ -12,4 +15,12 @@ public class AppConfig {
         return new DozerBeanMapperFactoryBean();
     }
 
+
+    /**
+     * 时区配置
+     */
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jacksonObjectMapperCustomization()  {
+        return jacksonObjectMapperBuilder -> jacksonObjectMapperBuilder.timeZone(TimeZone.getDefault());
+    }
 }
